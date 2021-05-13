422b8e9252c96ad39ed749f63470b719857db3b2
X-TermiNation <64397435+X-TermiNation@users.noreply.github.com>
2021-05-13 18:26:11 +0700

    udh ada login

---
+++ Proyek-PCS_toko/Proyek-PCS_toko/login.xaml.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using Oracle.DataAccess.Client;
+
+namespace Proyek_PCS_toko
+{
+    /// <summary>
+    /// Interaction logic for login.xaml
+    /// </summary>
+    public partial class login : Window
+    {
+        public login()
+        {
+            InitializeComponent();
+            conn = MainWindow.conn;
+            adddata();
+        }
+        public static string nmuser;
+        public static string current_idus;
+        OracleConnection conn;
+        Boolean log = false;
+        List<user> us = new List<user>();
+
+        private void adddata()
+        {
+            OracleCommand cmd = new OracleCommand($"select * from CUSTOMER", conn);
+            conn.Close();
+            conn.Open();
+            OracleDataReader reader = cmd.ExecuteReader();
+            while (reader.Read())
+            {
+                us.Add(new user(Convert.ToInt32(reader[0]), reader.GetString(1), reader.GetString(2), reader.GetString(3), reader.GetString(4), reader.GetString(5), Convert.ToInt32(reader[6])));
+            }
+            conn.Close();
+        }
+        private void btnlogin_Click(object sender, RoutedEventArgs e)
+        {
+            if (tbusername.Text.ToUpper() == "ADMIN" && tbpassword.Text.ToUpper() == "ADMIN")
+            {
+                MessageBox.Show("Berhasil login admin");
+                menu m = new menu();
+                this.Close();
+                m.Show();
+            }
+            else
+            {
+                for (int i = 0; i < us.Count; i++)
+                {
+                    if (tbusername.Text.ToUpper() == us[i].Username.ToUpper())
+                    {
+                        if (tbpassword.Text.ToUpper() == us[i].Password.ToUpper())
+                            log = true;
+                        else
+                            MessageBox.Show("password salah");
+                    }
+                }
+
+                if (log)
+                {
+                    MessageBox.Show("Berhasil login customer");
+                }
+                else
+                {
+                    MessageBox.Show("Username tidak ada / Username salah");
+                }
+            }
+        }
+    }
+}
---
+++ Proyek-PCS_toko/Proyek-PCS_toko/user.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proyek_PCS_toko
+{
+    class user
+    {
+        int id;
+        string username;
+        string nama;
+        string email;
+        string notelp;
+        string password;
+        int saldo;
+
+        public user(int id, string username, string nama, string email, string notelp, string password, int saldo)
+        {
+            this.id = id;
+            this.username = username;
+            this.nama = nama;
+            this.email = email;
+            this.notelp = notelp;
+            this.password = password;
+            this.saldo = saldo;
+        }
+
+        public int Id { get => id; set => id = value; }
+        public string Username { get => username; set => username = value; }
+        public string Nama { get => nama; set => nama = value; }
+        public string Email { get => email; set => email = value; }
+        public string Notelp { get => notelp; set => notelp = value; }
+        public string Password { get => password; set => password = value; }
+        public int Saldo { get => saldo; set => saldo = value; }
+    }
+}
